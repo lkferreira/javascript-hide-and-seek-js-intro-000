3d5bcde319988b89449b1d33f05f2a9e0cbe201e
Lucas Klefasz Ferreira <ferreirak.lucas@gmail.com>
2019-05-11 23:44:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  var nodes = document.querySelector('#grand-node div div div div')
-  return nodes[0]
+  return document.querySelector('#grand-node div div div div')
+  
 
 
 }

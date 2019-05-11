29c592854af2b1b1fd36af47491eb84aeb03ec64
Lucas Klefasz Ferreira <ferreirak.lucas@gmail.com>
2019-05-11 22:16:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return document.querySelector('#nested div div div div.target')
+  return document.querySelector('#nested div div div .target')
 }

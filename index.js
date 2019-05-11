9cefd48f1e12f9cb022eef52f03e9793b28f5fa4
Lucas Klefasz Ferreira <ferreirak.lucas@gmail.com>
2019-05-11 23:51:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested div div div .target')
+}
+
+function increaseRankBy(n) {
+  var lis = document.querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+}
+}
+
+function deepestChild() {
+  var node = document.querySelector('#grand-node div div div div')
+  return node
+}
